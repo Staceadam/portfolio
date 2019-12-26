19c1e356f97a1a80cc9c19f61cec2ba87825c8dd
Stacy Adam <stacy@Stacys-MacBook-Pro.local>
2019-12-26 15:28:20 -0600

    finishes styles and data

--- src/components/heroBox.js
+++ src/components/heroBox.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faGithub, faLinkedin, faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
 
-import './heroBox.scss'
+import './heroBox.scss';
 
 const HeroBox = () => {
-  return (
-    <header>
-      <div className="hero-text-box">
-        <img src={require('../images/meCropped.jpg')} alt="Stacy Adam" />
-        <h1>Stacy Adam</h1>
+	return (
+		<header>
+			<video class="header-video" width="100%" height="100%" autoplay loop>
+				<source src={require('../assets/coding.mp4')} type="video/mp4"></source>
+			</video>
 
-        <h2>Full Stack Software Developer</h2>
-        <ul className="social-links">
-          <li><a target="_blank" href="https://twitter.com/Stacyatom"><FontAwesomeIcon icon={faTwitter} className='fa-twitter' /></a></li>
-          <li><a target="_blank" href="https://github.com/Staceadam"><FontAwesomeIcon icon={faGithub} className='fa-github' /></a></li>
-          <li><a target="_blank" href="http://linkedin.com/in/stacy-adam"><FontAwesomeIcon icon={faLinkedin} className='fa-linkedin-square' /></a></li>
-          <li><a target="_blank" href="https://www.facebook.com/staceadam"><FontAwesomeIcon icon={faFacebookSquare} className='fa-facebook' /></a></li>
-        </ul>
-      </div>
-    </header>
-  )
-}
+			<div className="hero-text-box">
+				<img src={require('../assets/meCropped.jpg')} alt="Stacy Adam" />
+				<h1>Stacy Adam</h1>
+
+				<h2>Full Stack Software Developer</h2>
+				<ul className="social-links">
+					<li>
+						<a target="_blank" href="https://twitter.com/Stacyatom">
+							<FontAwesomeIcon icon={faTwitter} className="fa-twitter" />
+						</a>
+					</li>
+					<li>
+						<a target="_blank" href="https://github.com/Staceadam">
+							<FontAwesomeIcon icon={faGithub} className="fa-github" />
+						</a>
+					</li>
+					<li>
+						<a target="_blank" href="http://linkedin.com/in/stacy-adam">
+							<FontAwesomeIcon icon={faLinkedin} className="fa-linkedin-square" />
+						</a>
+					</li>
+					<li>
+						<a target="_blank" href="https://www.facebook.com/staceadam">
+							<FontAwesomeIcon icon={faFacebookSquare} className="fa-facebook" />
+						</a>
+					</li>
+				</ul>
+			</div>
+		</header>
+	);
+};
 
 export default HeroBox;
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Dialog } from '@blueprintjs/core';
+import { Dialog, Button } from '@blueprintjs/core';
 
 import './projects.scss';
 
@@ -14,47 +14,116 @@ const projectData = [
 			'Implemented new sign-in flow and Account Summary pages.',
 			`Released Blain's rewards program and Blain's Credit Card.`
 		],
-		link: 'https://www.farmandfleet.com'
+		link: {
+			title: 'farmandfleet.com',
+			href: 'https://www.farmandfleet.com'
+		}
 	},
 	{
 		name: 'Farm & Fleet Mobile',
 		platform: 'React Native Application',
-		className: 'fandfapp'
+		className: 'fandfapp',
+		features: [
+			'Part of the initial release team.',
+			'Created a custom Navigation solution after upgrading to latest navigation package.',
+			'Implemented a unique multiple Registration feature.',
+			'Responsible for maintenance and upkeep.'
+		],
+		link: {
+			title: 'iOS App Store',
+			href: 'https://apps.apple.com/us/app/blains-farm-fleet/id1293793816'
+		}
 	},
+
 	{
 		name: 'Small Shops United',
 		platform: 'React Native Application',
-		className: 'small-shops'
+		className: 'small-shops',
+		features: [
+			'Responsible for entire development process, start to app release.',
+			'Created all UI Components.',
+			'Implemented a custom location solution for individual merchants.',
+			'Created a data management solution from a legacy API using Redux/Redux-Thunk.',
+			'Created a custom Video Player for local content.'
+		],
+		link: {
+			title: 'iOS App Store',
+			href: 'https://apps.apple.com/us/app/ssu/id521587305?app=itunes&ign-mpt=uo%3D4'
+		}
 	},
 	{
 		name: 'Antonio Fred Hair',
 		platform: 'SquareSpace Website',
-		className: 'antonio'
+		className: 'antonio',
+		features: [
+			'Responsible for client communication, design and development.',
+			'Manually added site content including high res images.'
+		],
+		link: {
+			title: 'antoniofredhair.com',
+			href: 'http://www.antoniofredhair.com/'
+		}
 	},
 	{
 		name: 'Bobs Coins & Hobbies',
 		platform: 'Shopify Website',
-		className: 'bch'
+		className: 'bch',
+		features: [
+			'Responsible for client communication, design and development.',
+			'Manually added site content including high res images.',
+			'Trained Owners how to add products to site.'
+		],
+		link: {
+			title: 'bobsconsandhobbies.com',
+			href: 'https://bobscoinsandhobbies.com/'
+		}
 	},
 	{
 		name: 'Milwaukee Turnes',
 		platform: 'SquareSpace Website',
-		className: 'turners'
+		className: 'turners',
+		features: ['Added site content.', 'Created custom js solutions.'],
+		link: {
+			title: 'milwaukeeturners.org/',
+			href: 'http://www.milwaukeeturners.org/'
+		}
 	},
 	{
 		name: 'Movies App Server',
 		platform: 'GraphQL Server',
-		className: 'movie-server'
+		className: 'movie-server',
+		features: [
+			'Responsible for server and database architecture.',
+			'Created Postgres DB instance.',
+			'Uses Apollo Server.'
+		],
+		link: {
+			title: 'movies-app.herokuapp.com/graphql',
+			href: 'https://movies-app666.herokuapp.com/graphql'
+		}
 	},
 	{
 		name: 'Movies App Client',
 		platform: 'GraphQL Client',
-		className: 'movie-client'
+		className: 'movie-client',
+		features: [
+			'Uses Apollo Client for data fetching and state management.',
+			'Responsible for UI desgin and development.'
+		],
+		link: {
+			title: 'movies-appclient.herokuapp.com/',
+			href: 'https://movies-appclient666.herokuapp.com/'
+		}
 	},
 	{
 		name: 'Game Server',
 		platform: 'Express REST API',
-		className: 'game-server'
+		className: 'game-server',
+		features: ['Used best practices for node architecture.', 'Created Postgres DB instance.'],
+		link: {
+			title: 'game-server-cyw.herokuapp.com/api/v1/status',
+			href: 'https://game-server-cyw.herokuapp.com/api/v1/status'
+		}
 	}
 ];
 
@@ -94,6 +163,12 @@ const Projects = () => {
 							))}
 						</ul>
 						<p className="modal-header">Link</p>
+						<a target="_blank" href={activeModal.link.href}>
+							{activeModal.link.title}
+						</a>
+						<div class="button-wrapper">
+							<Button text="Okay." intent="primary" onClick={() => setActiveModal(null)} />
+						</div>
 					</div>
 				</Dialog>
 			)}
