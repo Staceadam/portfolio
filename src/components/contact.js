b983621b3df3320f0b811599894d4c142c987327
Stacy Adam <staceadam@gmail.com>
2020-03-30 09:28:09 -0500

    mobile contact width fix

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,27 +1,35 @@
-import React from 'react';
-import './contact.scss';
+import React from "react";
+import "./contact.scss";
 
 const Contact = () => {
-	return (
-		<section className="section-contact" id="contact">
-			<div className="row">
-				<h2>Contact</h2>
-			</div>
-			<div className="row">
-				<form method="POST" action="https://formspree.io/Staceadam@gmail.com" className="contact-form">
-					<label className="formHeader">Name</label>
-					<input type="text" name="name" id="name" required />
-					<label className="formHeader">Email</label>
-					<input type="email" name="_replyto" id="email" required />
-					<label className="formHeader">Question</label>
-					<textarea className="message" name="message"></textarea>
-					<div className="submitContainer">
-						<input className="contact-button" type="submit" value="Send" />
-					</div>
-				</form>
-			</div>
-		</section>
-	);
+    return (
+        <section className="section-contact" id="contact">
+            <div className="row">
+                <h2>Contact</h2>
+            </div>
+            <div className="row">
+                <form
+                    method="POST"
+                    action="https://formspree.io/Staceadam@gmail.com"
+                    className="contact-form"
+                >
+                    <label className="formHeader">Name</label>
+                    <input type="text" name="name" id="name" required />
+                    <label className="formHeader">Email</label>
+                    <input type="email" name="_replyto" id="email" required />
+                    <label className="formHeader">Question</label>
+                    <textarea className="message" name="message"></textarea>
+                    <div className="submitContainer">
+                        <input
+                            className="contact-button"
+                            type="submit"
+                            value="Send"
+                        />
+                    </div>
+                </form>
+            </div>
+        </section>
+    );
 };
 
 export default Contact;
